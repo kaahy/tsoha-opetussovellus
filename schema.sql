d67f365d2108d4b33c7a48084707eec2d1fec70e
kaa <113045348+kaahy@users.noreply.github.com>
2023-07-27 15:57:45 +0300

    Add schema

---
+++ schema.sql
@@ -0,0 +1,44 @@
+CREATE TABLE users (
+	id SERIAL PRIMARY KEY,
+	name TEXT UNIQUE,
+	password TEXT,
+	is_teacher BOOLEAN
+);
+
+CREATE TABLE courses (
+	id SERIAL PRIMARY KEY,
+	name TEXT,
+	user_id INTEGER REFERENCES users
+);
+
+CREATE TABLE course_pages (
+	id SERIAL PRIMARY KEY,
+	course_id INTEGER REFERENCES courses,
+	content TEXT
+);
+
+CREATE TABLE quizzes (
+	id SERIAL PRIMARY KEY,
+	course_page_id INTEGER REFERENCES course_pages,
+	question TEXT
+);
+
+CREATE TABLE choices (
+	id SERIAL PRIMARY KEY,
+	quiz_id INTEGER REFERENCES quizzes,
+	content TEXT,
+	is_correct BOOLEAN
+);
+
+CREATE TABLE results (
+	id SERIAL PRIMARY KEY,
+	user_id INTEGER REFERENCES users,
+	quiz_id INTEGER REFERENCES quizzes,
+	is_correct BOOLEAN
+);
+
+CREATE TABLE participants (
+	id SERIAL PRIMARY KEY,
+	user_id INTEGER REFERENCES users,
+	course_id INTEGER REFERENCES courses
+);
