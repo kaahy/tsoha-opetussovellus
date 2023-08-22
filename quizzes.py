b9f92c345009dc28c83d181162b08984900dc4d1
kaa <113045348+kaahy@users.noreply.github.com>
2023-08-22 15:45:14 +0300

    Fix bug

--- quizzes.py
+++ quizzes.py
@@ -21,10 +21,18 @@ def add_quiz(course_page_id, question, choices):
 
 def save_results(page_id, user_id):
     # saves only correct results, at least for now
-    quiz_ids = db.session.execute(text(f"SELECT id FROM quizzes WHERE course_page_id={page_id}"))    
+    quiz_ids = db.session.execute(text(f"SELECT id FROM quizzes WHERE course_page_id={page_id}")).fetchall()
+    quiz_ids = [quiz_ids[x][0] for x in range(len(quiz_ids))]
     for quiz_id in quiz_ids:
-        db.session.execute(text("INSERT INTO results (user_id, quiz_id, is_correct) VALUES ("+str(user_id)+", "+str(quiz_id[0])+", 't')"))
-    db.session.commit()
+        if not is_quiz_solved(quiz_id, user_id):
+            db.session.execute(text(f"INSERT INTO results (user_id, quiz_id, is_correct) VALUES ('{user_id}', '{quiz_id}', 't')"))
+            db.session.commit()
+
+def is_quiz_solved(quiz_id, user_id):
+    sql = f"SELECT COUNT(*) FROM results WHERE quiz_id={quiz_id} AND user_id={user_id} AND is_correct='t'"
+    if db.session.execute(text(sql)).fetchone()[0]:
+        return True
+    return False
 
 def check_quizzes(page_id, guesses):
     # student needs to answer correctly to all quizzes on the page
